3b683c90b27bb2974a4b28373f0fd16a01a32f81
Alastair McGilloway <alastairmcgilloway@hotmail.com>
2018-10-30 14:14:23 +0000

    Fixes pirate details

--- frontend/src/components/pirates/Pirate.js
+++ frontend/src/components/pirates/Pirate.js
@@ -3,35 +3,19 @@ import {Link} from 'react-router-dom';
 
 const Pirate = (props) => {
 
-  const onDelete = () => {
-    props.handleDelete(props.pirate.id);
-  }
-
-  const onEdit = () => {
-    props.handleEdit(props.pirate.id)
-  }
-
   if(!props.pirate){
     return null;
   }
-  const raids = props.pirate.raids.map((raid, index) => {
-    return <li key={index} >{raid.location}</li>
-  });
 
   return (
-    <div className="component">
+    <React.Fragment>
         <Link to = {"/pirates/" + props.pirate.id} className="name">
           {props.pirate.firstName} {props.pirate.lastName}
         </Link>
       <p>Age: {props.pirate.age}</p>
       <p>Ship: {props.pirate.ship.name}</p>
-      Raids:
-      <ul>
-        {raids}
-      </ul>
-      <button onClick={onDelete}>Delete Pirate</button>
-      <button onClick={onEdit}>Edit Pirate</button>
-    </div>
+
+    </React.Fragment>
   )
 }
 
